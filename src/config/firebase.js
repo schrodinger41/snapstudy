4202578b7924991d268c52ecf939dc9a1512c214
jhet <jhetdizon41@gmail.com>
2024-08-14 22:36:00 +0800

    firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCu1UPB4CbTA4dwHnZDf8D5dfXrCTshV3U",
@@ -14,3 +15,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+
+export const db = getFirestore(app);
---
+++ src/pages/cardPage/CardPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./cardPage.css";
+
+const CardPage = () => {
+  return <div>CardPage</div>;
+};
+
+export default CardPage;
